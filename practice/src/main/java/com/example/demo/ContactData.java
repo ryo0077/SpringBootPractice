2c30a93a31ab76b3e35be0c96fee92ffd1ccce13
ryo ishida <r.ishida.cingroup2012@gmail.com>
2025-10-06 21:54:24 +0900

    Lombok

---
+++ practice/src/main/java/com/example/demo/ContactData.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+
+public class ContactData {
+	private String lastName;
+	private String firstName;
+	private String email;
+	private String phone;
+	private String zipCode;
+	private String address;
+	private String buildingName;
+	private String contactType;
+	private String body;
+
+}
